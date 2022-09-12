6be06a12b33fd0e43caf1f6cfe2d3e304fc7d483
admontalvo <admontalvo@ufl.edu>
2022-09-12 10:08:36 -0400

    Creating sys_info_page.sh

--- sys_info_page.sh
+++ sys_info_page.sh
@@ -3,7 +3,10 @@ echo "<html>"
 echo " <head>"
 echo " <title>Page Title</title>"
 echo " </head>"
+
 echo " <body>"
 echo " Page body."
 echo " </body>"
+echo <h1>System Information Report</h1>
 echo "</html>"
+
